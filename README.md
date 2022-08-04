36b6daa1dbb716b1192ed4bee3c47f91b79cba44
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-08-04 20:10:35 +0400

    Package setup, directory.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Florgon Gatey Python SDK.
+
+Gatey Python SDK for API. Works with Florgon hosted, or self-hosted server.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gatey-sdk",
+    version="0.0.1",
+    author="Florgon Team and Contributors",
+    author_email="support@florgon.space",
+    description="Python client for Gatey (https://gatey.florgon.space)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    package_data={"gatey_sdk": ["py.typed"]},
+    install_requires=[
+        "requests>=2.28.1; python_version>='3.7'",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
