a8b4684b38717dcc8a34768faf554e6df57a6663
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-12-02 04:21:42 +0400

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Gatey Python SDK'
-copyright = '2022, Florgon Team and Contributors'
-author = 'Florgon Team and Contributors'
-release = '0.0.4'
+project = "Gatey Python SDK"
+copyright = "2022, Florgon Team and Contributors"
+author = "Florgon Team and Contributors"
+release = "0.0.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
