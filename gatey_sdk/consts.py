3d9269a0dbd7dadfa634ed601198b349721038d1
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2023-03-24 02:43:37 +0300

    fix(api_provider): Migrate to new domain.
    Signed-off-by: Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>

--- gatey_sdk/consts.py
+++ gatey_sdk/consts.py
@@ -7,7 +7,7 @@ from gatey_sdk.__version__ import __version__ as library_version
 # Default API server provider.
 # By default, this is API server provided from Florgon,
 # but user can override this with self-hosted provider.
-API_DEFAULT_SERVER_PROVIDER_URL = "https://api-gatey.florgon.space/v1"
+API_DEFAULT_SERVER_PROVIDER_URL = "https://api-gatey.florgon.com/v1"
 
 # Expected version from the API server.
 API_DEFAULT_SERVER_EXPECTED_VERSION = "0.0.0"
